74f34598d2007082323b06c94ad131683b34d647
Mohamad Kasiri <kasiri182@gmail.com>
2016-12-16 12:35:34 +0330

    Initial Commit

---
+++ NewDelino/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewDelino
+//
+//  Created by Mohammad on 12/16/16.
+//  Copyright © 2016 Mohammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
