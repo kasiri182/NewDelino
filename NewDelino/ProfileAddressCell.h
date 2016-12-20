46316b41003e7c4b6ef16438f793b8467d4072bb
Mohamad Kasiri <kasiri182@gmail.com>
2016-12-20 13:21:31 +0330

    second Versin : -make home page and search page -make profile page -make cart page

---
+++ NewDelino/ProfileAddressCell.h
@@ -0,0 +1,13 @@
+//
+//  ProfileAddressCell.h
+//  NewDelino
+//
+//  Created by Mohammad on 12/19/16.
+//  Copyright © 2016 Mohammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileAddressCell : UITableViewCell
+
+@end
---
+++ NewDelino/ProfileAddressCell.m
@@ -0,0 +1,24 @@
+//
+//  ProfileAddressCell.m
+//  NewDelino
+//
+//  Created by Mohammad on 12/19/16.
+//  Copyright © 2016 Mohammad. All rights reserved.
+//
+
+#import "ProfileAddressCell.h"
+
+@implementation ProfileAddressCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
