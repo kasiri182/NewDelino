a18dd34ae3417ec87c15c4ede1786a288cfe125d
Mohamad Kasiri <kasiri182@gmail.com>
2016-12-22 11:05:31 +0330

    update 3 : edit address ham zade shod

---
+++ NewDelino/MKTextView.h
@@ -0,0 +1,13 @@
+//
+//  MKTextView.h
+//  NewDelino
+//
+//  Created by Mohammad on 12/21/16.
+//  Copyright © 2016 Mohammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKTextView : UITextView
+
+@end
---
+++ NewDelino/MKTextView.m
@@ -0,0 +1,21 @@
+//
+//  MKTextView.m
+//  NewDelino
+//
+//  Created by Mohammad on 12/21/16.
+//  Copyright © 2016 Mohammad. All rights reserved.
+//
+
+#import "MKTextView.h"
+
+@implementation MKTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
