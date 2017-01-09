5538e1f63195cb205e2c67b0459f11aeeec6f256
Mohamad Kasiri <kasiri182@gmail.com>
2017-01-09 14:46:05 +0330

    update 5: location user gerefte mishe va hamchenin age location off bashe peigham mide - albate baiad error location handle beshe

---
+++ NewDelino/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  NewDelino
+//
+//  Created by Mohammad on 1/5/17.
+//  Copyright © 2017 Mohammad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
